33444bb7f4de48cd6a93f93e2a1559b7c8cea5c0
Luca <x3lit@outlook.com>
2018-11-01 18:30:41 +0100

    Updates

---
+++ precache-manifest.55510f84798f3ec9cb3ed5fc63dae42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e25bdafbe65ce95db67",
+    "url": "/pokefinder/static/js/runtime~main.7e25bdaf.js"
+  },
+  {
+    "revision": "a215b2f1451416fa0d24",
+    "url": "/pokefinder/static/js/main.a215b2f1.chunk.js"
+  },
+  {
+    "revision": "377f0c06f83a83fb5b5a",
+    "url": "/pokefinder/static/js/1.377f0c06.chunk.js"
+  },
+  {
+    "revision": "a215b2f1451416fa0d24",
+    "url": "/pokefinder/static/css/main.c8a10176.chunk.css"
+  },
+  {
+    "revision": "377f0c06f83a83fb5b5a",
+    "url": "/pokefinder/static/css/1.22cf5959.chunk.css"
+  },
+  {
+    "revision": "2f754b1e8ceaa613e633684eb9350723",
+    "url": "/pokefinder/index.html"
+  }
+];
